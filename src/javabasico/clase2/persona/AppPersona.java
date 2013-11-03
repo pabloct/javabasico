00abce63d245e8e6d13937460886d9efdc3ba83e
alumno <alumno@LAB706-20>
2013-11-02 19:12:55 -0500

    Proyecto base

---
+++ src/javabasico/clase2/persona/AppPersona.java
@@ -0,0 +1,20 @@
+package javabasico.clase2.persona;
+
+public class AppPersona {
+
+    public static void main(String[] args) {
+        Persona adrian = new Persona("Adrian Rodrigo Arturo", "Campos Liberato", 9);
+
+        System.out.println("Atruibutos del objeto");
+        System.out.println("Nombres: " + adrian.getNombres());
+        System.out.println("Apellidos: " + adrian.getApellidos());
+        System.out.println("Edad: " + adrian.getEdad());
+        
+        Persona ariana = new Persona();
+        ariana.setNombresApellidos("Ariana Valeria Constanza", "Campos Liberato");
+        ariana.printNombreCompleto();
+        ariana.setEdad(1);
+        System.out.println("Edad: " + ariana.getEdad());
+        
+    }
+}
